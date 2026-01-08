6dd6d0116f7f78769e9ad8ee379a034852b39682
eshwar075 <eshwxr065@gmail.com>
2026-01-09 00:57:21 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -6,7 +6,8 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# UPDATED IMPORT LINE BELOW
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import create_retrieval_chain
