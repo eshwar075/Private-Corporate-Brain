292ced6017bf47fc7b69e336db638e22f7cbdc46
eshwar075 <eshwxr065@gmail.com>
2026-01-09 01:30:30 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -7,13 +7,10 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
-
-# --- THE FIX IS HERE ---
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-# -----------------------
-
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
+# Standard imports that match the requirements above
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
